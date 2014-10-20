e08a8ef89c3d34ec61c48cfe28e4311f58cbfd9c
BartBien <phoenixjcam@gmail.com>
2014-10-20 09:15:44 +0200

    SOAP example

--- src/ProductCatalog.java
+++ src/ProductCatalog.java
@@ -4,6 +4,13 @@ import java.util.List;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
+/**
+ * https://www.youtube.com/watch?v=u5cQkVgq6jE&index=12&list=PLWvdZOFzHXEv5IXQjl7GS6RLxwrsP56wg
+ * 
+ * @author BartBien
+ *
+ */
+
 @WebService
 public class ProductCatalog
 {
